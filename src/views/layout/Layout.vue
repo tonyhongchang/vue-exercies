0de4dab73b05b8d9fac96f7b3e8b7de1e288896c
tonyhongchang <2252524611@qq.com>
2019-08-04 23:06:21 +0800

    update 19.08.04

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,7 +1,9 @@
 <template>
-  <div :class="classObj" class="app-wrapper">
+  <div class="app-wrapper">
     <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg">456</div>
-    123
+     <div class="main-container">
+      <navbar/>
+    </div>
   </div>
 
 
@@ -9,9 +11,11 @@
 
 <script>
 
-  export default {
+  import Navbar from "./components/Navbar";
 
+  export default {
     name: 'Layout',
+    components: {Navbar},
     computed: {
       device() {
         console.log(this.$store.state.app.device)
@@ -19,18 +23,24 @@
       },
       sidebar() {
         return this.$store.state.app.sidebar
-      },
-      classObj() {
-        return {
-          hideSidebar: !this.sidebar.opened,
-          openSidebar: this.sidebar.opened,
-          withoutAnimation: this.sidebar.withoutAnimation,
-          mobile: this.device === 'mobile'
-        }
       }
     }
   }
 
 </script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  @import "../../styles/mixin.scss";
+  .app-wrapper {
+    @include clearfix;
+    position: relative;
+    height: 100%;
+    width: 100%;
+    &.mobile.openSidebar{
+      position: fixed;
+      top: 0;
+    }
+  }
+</style>
 
 
